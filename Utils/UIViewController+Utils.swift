a97d6d551b46faa1f13f00a7b009af4132b9f94f
Hüseyin Temel <huseyintemeel@gmail.com>
2023-07-04 14:26:56 +0300

    add tab bar

---
+++ Utils/UIViewController+Utils.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Utils.swift
+//  bankey
+//
+//  Created by huseyin on 4.07.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+  
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
